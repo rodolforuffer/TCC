48d41e1b39fad8d5887b2abc166a6b27017d362f
rodolforuffer <rodolforuffer@yahoo.com.br>
2015-04-11 13:14:20 -0300

    Up

--- index.php
+++ index.php
@@ -1,65 +1,65 @@
 <?php
-session_start();
-require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php' );
-require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
-require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php' );
-require_once( 'lib/Facebook/Entities/AccessToken.php' );
-require_once( 'lib/Facebook/Entities/SignedRequest.php');
-require_once( 'lib/Facebook/FacebookSession.php' );
-require_once( 'lib/Facebook/FacebookSignedRequestFromInputHelper.php');
-require_once( 'lib/Facebook/FacebookCanvasLoginHelper.php');
-require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php' );
-require_once( 'lib/Facebook/FacebookRequest.php' );
-require_once( 'lib/Facebook/FacebookResponse.php' );
-require_once( 'lib/Facebook/FacebookSDKException.php' );
-require_once( 'lib/Facebook/FacebookRequestException.php' );
-require_once( 'lib/Facebook/FacebookOtherException.php' );
-require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
-require_once( 'lib/Facebook/GraphObject.php' );
-require_once( 'lib/Facebook/GraphUser.php');
-require_once( 'lib/Facebook/GraphSessionInfo.php' );
-
-use Facebook\HttpClients\FacebookHttpable;
-use Facebook\HttpClients\FacebookCurl;
-use Facebook\HttpClients\FacebookCurlHttpClient;
-use Facebook\Entities\AccessToken;
-use Facebook\Entities\SignedRequest;
-use Facebook\FacebookSession;
-use Facebook\FacebookSignedRequestFromInputHelper;
-use Facebook\FacebookCanvasLoginHelper;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
-use Facebook\FacebookResponse;
-use Facebook\FacebookSDKException;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookOtherException;
-use Facebook\FacebookAuthorizationException;
-use Facebook\GraphObject;
-use Facebook\GraphUser;
-use Facebook\GraphSessionInfo;
-
-$facebook = FacebookSession::setDefaultApplication('1626914787539438','a8be139b93eb4090e1527f6e25f91981');
-$helper = new FacebookCanvasLoginHelper();
-
-try {
-	$session = $helper->getSession();
-} catch (FacebookRequestException $ex) {
-	echo $ex->getMessage();
-} catch (\Exception $ex) {
-	echo $ex->getMessage();
-}
-
-if ($session) {
-	try {
-		 $request = new FacebookRequest($session, 'GET', '/me');
-		 $response = $request->execute();
-		 $me = $response->getGraphObject();
-		 echo $me->getProperty('name');
-		} catch(FacebookRequestException $e) {
-		echo $e->getMessage();
+/*	FACEBOOK LOGIN BASIC - PHP SDK V4.0
+ *	file 			- index.php
+ * 	Developer 		- Krishna Teja G S
+ *	Website			- http://packetcode.com/apps/fblogin-basic/
+ *	Date 			- 27th Sept 2014
+ *	license			- GNU General Public License version 2 or later
+*/
+/* INCLUSION OF LIBRARY FILEs*/
+	require_once( 'lib/Facebook/FacebookSession.php');
+	require_once( 'lib/Facebook/FacebookRequest.php' );
+	require_once( 'lib/Facebook/FacebookResponse.php' );
+	require_once( 'lib/Facebook/FacebookSDKException.php' );
+	require_once( 'lib/Facebook/FacebookRequestException.php' );
+	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
+	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
+	require_once( 'lib/Facebook/GraphObject.php' );
+	require_once( 'lib/Facebook/GraphUser.php' );
+	require_once( 'lib/Facebook/GraphSessionInfo.php' );
+	require_once( 'lib/Facebook/Entities/AccessToken.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
+	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
+/* USE NAMESPACES */
+	
+	use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\FacebookResponse;
+	use Facebook\FacebookSDKException;
+	use Facebook\FacebookRequestException;
+	use Facebook\FacebookAuthorizationException;
+	use Facebook\GraphObject;
+	use Facebook\GraphUser;
+	use Facebook\GraphSessionInfo;
+	use Facebook\FacebookHttpable;
+	use Facebook\FacebookCurlHttpClient;
+	use Facebook\FacebookCurl;
+/*PROCESS*/
+	
+	//1.Stat Session
+	 session_start();
+	//2.Use app id,secret and redirect url
+	 $app_id = '1626914787539438';
+	 $app_secret = 'a8be139b93eb4090e1527f6e25f91981';
+	 $redirect_url='http://tcc-data-mining.herokuapp.com/';
+	 
+	 //3.Initialize application, create helper object and get fb sess
+	 FacebookSession::setDefaultApplication($app_id,$app_secret);
+	 $helper = new FacebookRedirectLoginHelper($redirect_url);
+	 $sess = $helper->getSessionFromRedirect();
+	//4. if fb sess exists echo name 
+	 	if(isset($sess)){
+	 		//create request object,execute and capture response
+		$request = new FacebookRequest($sess, 'GET', '/me');
+		// from response get graph object
+		$response = $request->execute();
+		$graph = $response->getGraphObject(GraphUser::className());
+		// use graph object methods to get user details
+		$name= $graph->getName();
+		echo "hi $name";
+	}else{
+		//else echo login
+		echo '<a href='.$helper->getLoginUrl().'>Login with facebook</a>';
 	}
-} else {
-	$helper = new FacebookRedirectLoginHelper('https://apps.facebook.com/tccmineracaodedados/');
-	$auth_url = $helper->getLoginUrl(array('email'));
-	echo "<script>window.top.location.href='".$auth_url."'</script>";
-}
