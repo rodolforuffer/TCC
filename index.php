54a0d9f1a83e9fcbf1735f0ec0c53ec72bef1e4b
rodolforuffer <rodolforuffer@yahoo.com.br>
2015-04-11 10:41:57 -0300

    Up

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
 <?php
-// incluir o sdk do facebook
-require 'facebook-php-sdk-master/src/facebook.php';
-  
-// Configure abaixo o appId e secret do seu aplicativo
+
+// Cria a instancia da aplicacao, informando o appid e o secret
 $facebook = new Facebook(array(
-  'appId'  => '1626914787539438',
-  'secret' => 'a8be139b93eb4090e1527f6e25f91981',
+  '1626914787539438'  => $APP_ID,
+  'a8be139b93eb4090e1527f6e25f91981' => $SECRET,
 ));
-  
-// verifica se o usuário já esta logado no aplicativo
+
+// obtem o id do usuario
 $user = $facebook->getUser();
-if ($user) { 
+
+if ($user) { // usuario logado
         try {
-        // Obtem dados do profile do usuario logado
-        // o app terá acesso somente os dados públicos
-            $user_profile = $facebook->api('/me');
-  
-            // exibe foto do usuario 
-            echo "<img src='https://graph.facebook.com/$user/picture' />";
-  
-            // printa os dados públicos do profile do usuario 
+        $permissions = $facebook->api("/me/permissions");
+
+        // tratar permissoes
+        $permissions_granted = array();
+        foreach($permissions['data'] as $perm) {
+                array_push($permissions_granted, $perm['permission']);
+        }
+
+        if(!in_array('email', $permissions_granted)
+                || !in_array('user_likes', $permissions_granted)
+                || !in_array('publish_actions', $permissions_granted)) {
+
+                header("Location: " . $facebook->getLoginUrl(array("scope" => "email, user_likes, publish_actions")));
+                exit;
+        }
+
+        // Obtem dados do usuario logado
+        $user_profile = $facebook->api('/me');
+
+        // exibe foto do usuario logado
+        echo "<img src=\"https://graph.facebook.com/$user/picture\">";
+
+        // printa os dados do profile do usuario logado
         echo "<pre>";
         print_r($user_profile);
         echo "</pre>";
-  
+
         } catch (FacebookApiException $e) {
-        // tratamento de erro
-                print_r($e);
+                error_log($e);
+                $user = null;
         }
 } else {
-        // usuario não logado, solicitar autenticação
-        $loginUrl = $facebook->getLoginUrl();
-        echo "<a href='$loginUrl'>Conectar no aplicativo</a><br />";
-        echo "<strong><em>Voc&ecirc; n&atilde;o esta conectado..</em></strong>";
+        // usuario nao logado, solicitar autenticacao e permissao
+        $loginUrl = $facebook->getLoginUrl(array("scope" => "email, user_likes, publish_actions"));
+        header("Location: $loginUrl");
 }
