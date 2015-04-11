24f99e98a2081decf63260e47d2ca1aab553df99
rodolforuffer <rodolforuffer@yahoo.com.br>
2015-04-11 00:34:23 -0300

    New Canvas

--- index.php
+++ index.php
@@ -1,71 +1,97 @@
 <?php
-/*	FACEBOOK LOGIN BASIC - PHP SDK V4.0
- *	file 			- index.php
- * 	Developer 		- Krishna Teja G S
- *	Website			- http://packetcode.com/apps/fblogin-basic/
- *	Date 			- 27th Sept 2014
- *	license			- GNU General Public License version 2 or later
-*/
-
-/* INCLUSION OF LIBRARY FILEs*/
-	require_once( 'lib/Facebook/FacebookSession.php');
-	require_once( 'lib/Facebook/FacebookRequest.php' );
-	require_once( 'lib/Facebook/FacebookResponse.php' );
-	require_once( 'lib/Facebook/FacebookSDKException.php' );
-	require_once( 'lib/Facebook/FacebookRequestException.php' );
-	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
-	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
-	require_once( 'lib/Facebook/GraphObject.php' );
-	require_once( 'lib/Facebook/GraphUser.php' );
-	require_once( 'lib/Facebook/GraphSessionInfo.php' );
-	require_once( 'lib/Facebook/Entities/AccessToken.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
-	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
-
-/* USE NAMESPACES */
-
-	use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\FacebookResponse;
-	use Facebook\FacebookSDKException;
-	use Facebook\FacebookRequestException;
-	use Facebook\FacebookAuthorizationException;
-	use Facebook\GraphObject;
-	use Facebook\GraphUser;
-	use Facebook\GraphSessionInfo;
-	use Facebook\FacebookHttpable;
-	use Facebook\FacebookCurlHttpClient;
-	use Facebook\FacebookCurl;
-
-/*PROCESS*/
-
-	 FacebookSession::setDefaultApplication('1626914787539438','a8be139b93eb4090e1527f6e25f91981');
-	 $helper = new FacebookCanvasLoginHelper();
-	 
-	 try{
-	 	$session = $helper->getSession();
-	 }catch (FacebookRequestException $e){
-	 	echo  $getMessage();
-	 }catch (\Exception $ex){
-	 	echo $e->getMessage();
-	 }
-	 
-	 if ($session){
-	 	try{
-	 		$request = new FacebookRequest($session, 'GET', '/me');
-	 		$response = $request->execute();
-	 		$me = $response->getGraphObject();
-	 		echo $me->getProperty('name');
-	 	}catch (FacebookRequestException $e){
-	 		echo $e->getMessage();
-	 	}
-	 }else{
-	 	$helper = new FacebookRedirectLoginHelper('https://apps.facebook.com/tccmineracaodedados/');
-	 	$auth_url = $helper->getLoginUrl(array('email','user_friends'));
-	 	echo "<script>window.top.location.href='".$auth_url."'</script>";
-	 }
-	 
-	 ?>
-
+// Working canvas APP, FB SDK 4.0
+session_start();
+ 
+// Load SDK Assets
+// Minimum required
+require_once 'Facebook/FacebookSession.php';
+require_once 'Facebook/FacebookRequest.php';
+require_once 'Facebook/FacebookResponse.php';
+require_once 'Facebook/FacebookSDKException.php';
+require_once 'Facebook/FacebookCanvasLoginHelper.php';
+require_once 'Facebook/GraphObject.php';
+require_once 'Facebook/GraphUser.php';
+require_once 'Facebook/GraphSessionInfo.php';
+require_once 'Facebook/HttpClients/FacebookHttpable.php';
+require_once 'Facebook/HttpClients/FacebookCurl.php';
+require_once 'Facebook/HttpClients/FacebookCurlHttpClient.php';
+ 
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Faceboob\FacebookSDKException;
+use Facebook\FacebookCanvasLoginHelper;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
+use Facebook\GraphSessionInfo;
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+$app_id = '1626914787539438';
+$app_secret = 'a8be139b93eb4090e1527f6e25f91981';
+$app_namespace = 'tccmineracaodedados';
+$app_scope = 'user_location,email';
+// Facebook APP keys
+FacebookSession::setDefaultApplication($app_id,$app_secret);
+// Helper for fb canvas authentication
+$helper = new FacebookCanvasLoginHelper();
+// see if  $_SESSION exists
+if (isset($_SESSION) && isset($_SESSION['fb_token']))
+{
+	// create new fb session from saved fb_token
+	$session = new FacebookSession($_SESSION['fb_token']);
+	// validate the fb_token to make sure it's still valid
+	try 
+	{
+    	    $session->validate();
+	}
+	catch (Exception $e)
+	{
+	    // catch any exceptions
+	    $session = null;
+	}
+}
+else
+{
+	// no $_SESSION exists
+	try
+	{
+		// create fb session
+		$session = $helper->getSession();
+	}
+	catch(FacebookRequestException $ex)
+	{
+		// When Facebook returns an error
+		//print_r($ex);
+		$session = null;
+	}
+	catch(\Exception $ex)
+	{
+		// When validation fails or other local issues
+		//print_r($ex);
+		$session = null;
+	}
+}
+// check if 1 of the 2 methods above set $session
+if (isset($session))
+{
+	// Lets save fb_token for later authentication through saved $_SESSION
+	$_SESSION['fb_token'] = $session->getToken();
+	// Logged in
+	$fb_me = (new FacebookRequest(
+	  $session, 'GET', '/me'
+	))->execute()->getGraphObject();
+	// We can get some info about the user
+	$fb_location_name = $fb_me->getProperty('location')->getProperty('name');
+	$fb_email = $fb_me->getProperty('email');
+	$fb_uuid = $fb_me->getProperty('id');
+}
+else
+{
+	session_destroy();
+	// We use javascript because of facebook bug https://developers.facebook.com/bugs/722275367815777
+	// Fix from here: http://stackoverflow.com/a/23685616/796443
+	// IF bug is fixed this line won't be needed, as app will ask for permissions onload without JS redirect.
+	$oauthJS = "window.top.location = 'https://www.facebook.com/dialog/oauth?client_id=$app_id&redirect_uri=https://apps.facebook.com/$app_namespace/&scope=$app_scope';";
+}
+?>
