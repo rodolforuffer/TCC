c2a7a84554c3ef5aacb17c871b35419e0951a10b
rodolforuffer <rodolforuffer@yahoo.com.br>
2015-04-12 23:30:17 -0300

    Up

--- index.php
+++ index.php
@@ -1,74 +1,88 @@
 <?php
-/*	FACEBOOK LOGIN BASIC - PHP SDK V4.0
- *	file 			- index.php
- * 	Developer 		- Krishna Teja G S
- *	Website			- http://packetcode.com/apps/fblogin-basic/
- *	Date 			- 27th Sept 2014
- *	license			- GNU General Public License version 2 or later
-*/
-/* INCLUSION OF LIBRARY FILEs*/
-	require_once( 'lib/Facebook/FacebookSession.php');
-	require_once( 'lib/Facebook/FacebookRequest.php' );
-	require_once( 'lib/Facebook/FacebookResponse.php' );
-	require_once( 'lib/Facebook/FacebookSDKException.php' );
-	require_once( 'lib/Facebook/FacebookRequestException.php' );
-	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
-	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
-	require_once( 'lib/Facebook/GraphObject.php' );
-	require_once( 'lib/Facebook/GraphUser.php' );
-	require_once( 'lib/Facebook/GraphSessionInfo.php' );
-	require_once( 'lib/Facebook/Entities/AccessToken.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
-	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
+/*
+ * FACEBOOK LOGIN BASIC - PHP SDK V4.0
+ * file - index.php
+ * Developer - Krishna Teja G S
+ * Website - http://packetcode.com/apps/fblogin-basic/
+ * Date - 27th Sept 2014
+ * license - GNU General Public License version 2 or later
+ */
+/* INCLUSION OF LIBRARY FILEs */
+require_once ('lib/Facebook/FacebookSession.php');
+require_once ('lib/Facebook/FacebookRequest.php');
+require_once ('lib/Facebook/FacebookResponse.php');
+require_once ('lib/Facebook/FacebookSDKException.php');
+require_once ('lib/Facebook/FacebookRequestException.php');
+require_once ('lib/Facebook/FacebookRedirectLoginHelper.php');
+require_once ('lib/Facebook/FacebookAuthorizationException.php');
+require_once ('lib/Facebook/GraphObject.php');
+require_once ('lib/Facebook/GraphUser.php');
+require_once ('lib/Facebook/GraphSessionInfo.php');
+require_once ('lib/Facebook/Entities/AccessToken.php');
+require_once ('lib/Facebook/HttpClients/FacebookCurl.php');
+require_once ('lib/Facebook/HttpClients/FacebookHttpable.php');
+require_once ('lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
 /* USE NAMESPACES */
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
+use Facebook\GraphSessionInfo;
+use Facebook\FacebookHttpable;
+use Facebook\FacebookCurlHttpClient;
+use Facebook\FacebookCurl;
+/* PROCESS */
+
+// 1.Stat Session
+session_start ();
+// 2.Use app id,secret and redirect url
+$app_id = '1626914787539438';
+$app_secret = 'a8be139b93eb4090e1527f6e25f91981';
+$redirect_url = 'http://tcc-data-mining.herokuapp.com/';
+
+// 3.Initialize application, create helper object and get fb sess
+FacebookSession::setDefaultApplication ( $app_id, $app_secret );
+$helper = new FacebookCanvasLoginHelper ();
+
+try {
 	
-	use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\FacebookResponse;
-	use Facebook\FacebookSDKException;
-	use Facebook\FacebookRequestException;
-	use Facebook\FacebookAuthorizationException;
-	use Facebook\GraphObject;
-	use Facebook\GraphUser;
-	use Facebook\GraphSessionInfo;
-	use Facebook\FacebookHttpable;
-	use Facebook\FacebookCurlHttpClient;
-	use Facebook\FacebookCurl;
-/*PROCESS*/
+	$sess = $helper->getSession ();
 	
-	//1.Stat Session
-	 session_start();
-	//2.Use app id,secret and redirect url
-	 $app_id = '1626914787539438';
-	 $app_secret = 'a8be139b93eb4090e1527f6e25f91981';
-	 $redirect_url='http://tcc-data-mining.herokuapp.com/';
-	 
-	 //3.Initialize application, create helper object and get fb sess
-	 FacebookSession::setDefaultApplication($app_id,$app_secret);
-	 $helper = new FacebookRedirectLoginHelper($redirect_url);
-	 $sess = $helper->getSessionFromRedirect();
-	//4. if fb sess exists echo name 
-	 	if(isset($sess)){
-		//Acess long token
-		// User logged in, get the AccessToken entity.
-        $accessToken = $session->getAccessToken();
-        // Exchange the short-lived token for a long-lived token.
-        $longLivedAccessToken = $accessToken->extend();
-        // Now store the long-lived token in the database
-        // . . . $db->store($longLivedAccessToken);
-        // Make calls to Graph with the long-lived token.
-        // . . . 
-	 		//create request object,execute and capture response
-		$request = new FacebookRequest($sess, 'GET', '/me');
-		// from response get graph object
-		$response = $request->execute();
-		$graph = $response->getGraphObject(GraphUser::className());
-		// use graph object methods to get user details
-		$name= $graph->getName();
-		echo "hi $name";
-	}else{
-		//else echo login
-		echo '<a href='.$helper->getLoginUrl(array('email, user_friends')).'>Login with facebook</a>';
-	}
+} catch ( FacebookRequestException $ex ) {
+	
+}catch (\Exception $ex){
+	
+}
+
+// 4. if fb sess exists echo name
+if (isset ( $sess )) {
+	// Acess long token
+	// User logged in, get the AccessToken entity.
+	$accessToken = $session->getAccessToken ();
+	// Exchange the short-lived token for a long-lived token.
+	$longLivedAccessToken = $accessToken->extend ();
+	// Now store the long-lived token in the database
+	$db->store ( $longLivedAccessToken );
+	// Make calls to Graph with the long-lived token.
+	// . . .
+	// create request object,execute and capture response
+	$request = new FacebookRequest ( $sess, 'GET', '/me' );
+	// from response get graph object
+	$response = $request->execute ();
+	$graph = $response->getGraphObject ( GraphUser::className () );
+	// use graph object methods to get user details
+	$name = $graph->getName ();
+	echo "hi $name";
+	
+} else {
+	// else echo login
+	echo '<a href=' . $helper->getLoginUrl ( array (
+			'email, user_friends' 
+	) ) . '>Login with facebook</a>';
+}
