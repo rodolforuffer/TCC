252936857eeee5f8545a3f7e1d3ce68958afaff0
rodolforuffer <rodolforuffer@yahoo.com.br>
2015-04-12 23:52:43 -0300

    up

--- index.php
+++ index.php
@@ -7,35 +7,45 @@
  *	license			- GNU General Public License version 2 or later
 */
 /* INCLUSION OF LIBRARY FILEs*/
-	require_once( 'lib/Facebook/FacebookSession.php');
-	require_once( 'lib/Facebook/FacebookRequest.php' );
-	require_once( 'lib/Facebook/FacebookResponse.php' );
-	require_once( 'lib/Facebook/FacebookSDKException.php' );
-	require_once( 'lib/Facebook/FacebookRequestException.php' );
-	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
-	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
-	require_once( 'lib/Facebook/GraphObject.php' );
-	require_once( 'lib/Facebook/GraphUser.php' );
-	require_once( 'lib/Facebook/GraphSessionInfo.php' );
-	require_once( 'lib/Facebook/Entities/AccessToken.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
-	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
+require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php' );
+require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
+require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php' );
+require_once( 'lib/Facebook/Entities/AccessToken.php' );
+require_once( 'lib/Facebook/Entities/SignedRequest.php');
+require_once( 'lib/Facebook/FacebookSession.php' );
+require_once( 'lib/Facebook/FacebookSignedRequestFromInputHelper.php');
+require_once( 'lib/Facebook/FacebookCanvasLoginHelper.php');
+require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'lib/Facebook/FacebookRequest.php' );
+require_once( 'lib/Facebook/FacebookResponse.php' );
+require_once( 'lib/Facebook/FacebookSDKException.php' );
+require_once( 'lib/Facebook/FacebookRequestException.php' );
+require_once( 'lib/Facebook/FacebookOtherException.php' );
+require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
+require_once( 'lib/Facebook/GraphObject.php' );
+require_once( 'lib/Facebook/GraphUser.php');
+require_once( 'lib/Facebook/GraphSessionInfo.php' );
+
 /* USE NAMESPACES */
-	
-	use Facebook\FacebookSession;
-	use Facebook\FacebookRedirectLoginHelper;
-	use Facebook\FacebookRequest;
-	use Facebook\FacebookResponse;
-	use Facebook\FacebookSDKException;
-	use Facebook\FacebookRequestException;
-	use Facebook\FacebookAuthorizationException;
-	use Facebook\GraphObject;
-	use Facebook\GraphUser;
-	use Facebook\GraphSessionInfo;
-	use Facebook\FacebookHttpable;
-	use Facebook\FacebookCurlHttpClient;
-	use Facebook\FacebookCurl;
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\Entities\AccessToken;
+use Facebook\Entities\SignedRequest;
+use Facebook\FacebookSession;
+use Facebook\FacebookSignedRequestFromInputHelper;
+use Facebook\FacebookCanvasLoginHelper;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookOtherException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
+use Facebook\GraphSessionInfo;
+
 /*PROCESS*/
 	
 	//1.Stat Session
