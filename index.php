7ddcec847dd3b6d702f8ebcb936e19a9bf773dfa
rodolforuffer <rodolforuffer@yahoo.com.br>
2015-04-13 00:09:42 -0300

    up

--- index.php
+++ index.php
@@ -57,8 +57,11 @@ use Facebook\GraphSessionInfo;
 	 
 	 //3.Initialize application, create helper object and get fb sess
 	 FacebookSession::setDefaultApplication($app_id,$app_secret);
-	 $helper = new FacebookRedirectLoginHelper($redirect_url);
-	 $sess = $helper->getSessionFromRedirect();
+     $helper = new FacebookCanvasLoginHelper();
+     
+	 $sess = $helper->getSession();
+  
+}
 	//4. if fb sess exists echo name 
 	 	if(isset($sess)){
 		//Acess long token
